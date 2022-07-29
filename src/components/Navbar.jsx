3752053015d933a4c11c56adc12bda25b5e2293d
starydv7 <pawanstar72926@gmail.com>
2022-07-29 23:57:20 +0530

    NAVBAR

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,15 @@ const Navbar = () => {
       <div className='navbar'>
         <div className='navbar1'> 
           <img src='http://www.sise.in/images/logo.png' />
-          
+          <div className='navbar2'>
+            <button>ABOUT</button>
+            <button>TALENT MANAGEMENT</button>
+            <button>DIGITAL MARKRTING</button>
+            <button>SPONSORPHIP SALES AND BRANDING</button>
+            <button>MUSIC</button>
+            <button><br>CONTACT</br></button>
+
+          </div>
         </div>
             
           </div>  
