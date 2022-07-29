1acf5c4b1fea8fb4a05050790a1499f738a314b4
starydv7 <pawanstar72926@gmail.com>
2022-07-29 23:31:08 +0530

    sise

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../css/navbar.css";
+const Navbar = () => {
+  return (
+      <div>
+          <div className='navbar'>
+              
+          </div>  
+    </div>
+  )
+}
+
+export default Navbar
