c527400c3161e58bf8ff093342cb652f6b8098b2
starydv7 <pawanstar72926@gmail.com>
2022-07-30 00:46:47 +0530

    color

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
       <div>
           <div className='homepage1'>
-              
+              <div className='sise1'>SECOND INNING</div>
           </div>
     </div>
   )
